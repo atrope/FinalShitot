cf52e4726f181b63e8b98325e86b8dc5382b26c6
Alan Trope <atrope@users.noreply.github.com>
2018-07-22 12:43:55 +0300

    fixed untracked files

--- FinalProject/Source.cpp
+++ FinalProject/Source.cpp
@@ -1,13 +1,11 @@
 #include "../Common/Graphics.h"
-#include "../Controls/Label.h"
+#include "../Common/Label.h"
 #include "../Common/EventEngine.h"
 
 int main(int argc, char** argv)
 {
-
 	Label l("Hello World");
 	Control& l2 = l;
 	EventEngine e;
 	e.run(l);
-
 }
