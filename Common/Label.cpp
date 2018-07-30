ffdde9255eafd57599324da8419a610e05ec64ca
Dolev Farhi <dolevfarhi636@gmail.com>
2018-07-30 18:17:10 +0300

    add comment label

--- Common/Label.cpp
+++ Common/Label.cpp
@@ -1,20 +1,20 @@
 #include "Label.h"
 #include <iostream>
 
-Label::Label(string value) : Control(), value(value) {};
+Label::Label(string value) : Control(), value(value) {}; //constuctor
 
-string Label::getValue()
+string Label::getValue()//get value
 {
     return this->value;
 }
 
-void Label::setValue(string value)
+void Label::setValue(string value) //set value
 {
     this->value = value;
 }
 
 
-void Label::draw(Graphics& g, int x, int y, size_t z)
+void Label::draw(Graphics& g, int x, int y, size_t z) //cheack if the size is good and then write function
 {
     if (!z) g.write(value);
 }
